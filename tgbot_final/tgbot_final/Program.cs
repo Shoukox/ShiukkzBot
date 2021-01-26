647c258e0abdee2ca34742e0d23c46b2cc5b0b3d
Shoukox <58671746+Shoukox@users.noreply.github.com>
2021-01-27 02:12:00 +0500

    Update Program.cs

--- tgbot_final/tgbot_final/Program.cs
+++ tgbot_final/tgbot_final/Program.cs
@@ -8,9 +8,7 @@ namespace tgbot_final
         static void Main(string[] args)
         {
             System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("en-US");
-            //1328219094:AAFjFsz80--0N_hT1K1-FRXVpkQvyQdGAWA fake
-            //1077875912:AAGoe-3mixyIv22Rf7IWHmstIc-Qh8bunb4 real
-            string token = "1328219094:AAFjFsz80--0N_hT1K1-FRXVpkQvyQdGAWA";
+            string token = "YOUR_TOKEN";
             while (true)
             {
                 BotMain botInstance = new BotMain(token);
